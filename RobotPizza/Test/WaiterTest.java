6cefaf94699fda818192f72d3e545c01aab3e91a
CPS 499 Dayton Spring 2017 <tmasthay1@udayton.edu>
2017-04-18 16:08:26 -0400

    Create WaiterTest.java

---
+++ RobotPizza/Test/WaiterTest.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurant;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tyler Masthay
+ */
+public class WaiterTest {
+    
+    public WaiterTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getName method, of class Waiter.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName is SIMPLE");
+        /*Waiter instance = null;
+        String expResult = "";
+        String result = instance.getName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");*/
+    }
+
+    /**
+     * Test of greetCustomer method, of class Waiter.
+     */
+    @Test
+    public void testGreetCustomer() {
+        System.out.println("greetCustomer is VOID");
+        /*String name = "";
+        Waiter instance = null;
+        instance.greetCustomer(name);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");*/
+    }
+
+    /**
+     * Test of correct method, of class Waiter.
+     */
+    @Test
+    public void testCorrect() {
+        System.out.println("correct is VOID");
+    }
+
+    /**
+     * Test of pinOrder method, of class Waiter.
+     */
+    @Test
+    public void testPinOrder() {
+        System.out.println("pinOrder is VOID");
+    }
+
+    /**
+     * Test of serve method, of class Waiter.
+     */
+    @Test
+    public void testServe() {
+        System.out.println("serve is VOID");
+    }
+
+    /**
+     * Test of goodbye method, of class Waiter.
+     */
+    @Test
+    public void testGoodbye() {
+        System.out.println("goodbye is VOID");
+    }
+    
+}
