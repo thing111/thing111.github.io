13bcd2de67aacc812e27da6ba693b9b467402085
CPS 499 Dayton Spring 2017 <tmasthay1@udayton.edu>
2017-04-18 16:01:19 -0400

    Create Waiter.java

---
+++ RobotPizza/Waiter.java
@@ -0,0 +1,34 @@
+package restaurant;
+
+public class Waiter {
+    private String name;
+    
+    Waiter(String tom) {
+        
+    }
+    
+    public String getName(){
+        return "nope";
+    }
+
+    void greetCustomer(String name) {
+        
+    }
+
+    void correct(String name) {
+        
+    }
+
+    void pinOrder(Chef chef, String item1) {
+        
+    }
+
+    void serve(String item1, String name) {
+        
+    }
+
+    void goodbye() {
+        
+    }
+    
+}
