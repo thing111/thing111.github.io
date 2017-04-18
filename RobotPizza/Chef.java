bca748f9419b98e5946cb370c4a6f842e11bf136
CPS 499 Dayton Spring 2017 <tmasthay1@udayton.edu>
2017-04-18 15:59:14 -0400

    Create Chef.java

---
+++ RobotPizza/Chef.java
@@ -0,0 +1,18 @@
+package restaurant;
+
+public class Chef {
+
+    private String name;
+    
+    Chef(String theName) {
+        
+    }
+    
+    public String getName(){
+        return "nope";
+    }
+    
+    public String cook(String item){
+        return "done";
+    }
+}
