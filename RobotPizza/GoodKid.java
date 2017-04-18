c2698f165c76aa86bea0cf01f003eec5c18ea4c0
CPS 499 Dayton Spring 2017 <tmasthay1@udayton.edu>
2017-04-18 16:00:18 -0400

    Create GoodKid.java

---
+++ RobotPizza/GoodKid.java
@@ -0,0 +1,18 @@
+package restaurant;
+
+public class GoodKid {
+
+    private String name;
+    
+    GoodKid(String theName) {
+        
+    }
+    
+    public String getName(){
+        return "nope";
+    }
+    
+    public String order(){
+        return "nope";
+    }
+}
