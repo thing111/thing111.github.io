bc23c2727e466d3bc9058b8bf8178a487686550f
CPS 499 Dayton Spring 2017 <tmasthay1@udayton.edu>
2017-04-18 16:05:38 -0400

    Update Chef.java

--- RobotPizza/Chef.java
+++ RobotPizza/Chef.java
@@ -3,6 +3,7 @@ package restaurant;
 public class Chef {
 
     private String name;
+    ArrayList<String> pendingOrders = new ArrayList<>();
     
     Chef(String theName) {
         
@@ -14,5 +15,9 @@ public class Chef {
     
     public String cook(String item){
         return "done";
+    }
+    
+    public void addOrder(String item){
+    
     }
 }
