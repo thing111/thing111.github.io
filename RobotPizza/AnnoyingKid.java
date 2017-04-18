3851170d542d5092551af2279c513d94da9986e1
CPS 499 Dayton Spring 2017 <tmasthay1@udayton.edu>
2017-04-18 15:58:53 -0400

    Create AnnoyingKid.java

---
+++ RobotPizza/AnnoyingKid.java
@@ -0,0 +1,23 @@
+package restaurant;
+
+public class AnnoyingKid {
+
+    private String theName;
+    
+    AnnoyingKid(String theName) {
+        
+    }
+    
+    public String politelyOrder(){
+        return "nope";
+    }
+    
+    public String misbehave(){
+        return "nope";
+    }
+
+    String getName() {
+        return "nope";
+    }
+    
+}
